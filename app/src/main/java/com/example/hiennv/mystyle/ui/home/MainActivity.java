bb59cc703da585503bc947b48273839f8b9bd223
hiencoder <mrphonglinh2605@gmail.com>
2018-10-15 16:37:21 +0700

    Commit DialogLoading

--- app/src/main/java/com/example/hiennv/mystyle/ui/home/MainActivity.java
+++ app/src/main/java/com/example/hiennv/mystyle/ui/home/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.hiennv.mystyle.ui.home;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+    }
+
+    private void handleOpenFrontCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra("android.intent.extras.CAMERA_FACING",1);
     }
 }
